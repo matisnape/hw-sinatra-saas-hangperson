5162db3b557dd4aca0fe0aa049283c5d9639e064
Anna Anks Nowak <matisnape@wp.pl>
2018-12-10 21:26:02 +0100

    Add validation for empty string

--- app.rb
+++ app.rb
@@ -39,7 +39,9 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    if @game.guesses.include?(letter) || @game.wrong_guesses.include?(letter)
+    if letter == ''
+      flash[:message] = 'Invalid guess'
+    elsif @game.guesses.include?(letter) || @game.wrong_guesses.include?(letter)
       flash[:message] = 'You have already used that letter'
     else
       @game.guess(letter)
